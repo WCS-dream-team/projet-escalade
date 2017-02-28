dd7020f6a88eae63545ccf7d323778960bc569c2
Christophe_vinc <vincentchristophe177@gmail.com>
2017-02-28 11:15:16 +0100

    Le footer version pour merge

--- footer.php
+++ footer.php
@@ -1,36 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-	<LINK rel="stylesheet" type="text/css" href="footer.css">
-</head>
-<body>
+
+
 <footer class="navbar-default">
- 	<div class="container">
+  <div class="container">
   
-  		<div class=”row bottom-rule”>
-  		<!-- 1st col of the row -->
-   			<div class="col-sm-4 footer-section">
-    			<strong>Tell us more about you and ask a question</strong>
-    			<p>first, write your address</p>
-    				<form class="form-inline">
-        		<div class="form-group">
-            		<label 
-                		class="sr-only" 
-                		for="inputEmail"
-            			>Email</label>
-            			<input
-                		type="email"
-                		class="form-control"
-                		id="inputEmail"
-                		placeholder="address@example.com">
-         		</div>
-         		<button type="submit" class="btn btn-default">Send</button>
-     				</form>
- 			</div>
+      <div class=”row bottom-rule”>
+      <!-- 1st col of the row -->
+        <div class="col-sm-4 footer-section">
+          <strong>Tell us more about you and ask a question</strong>
+          <p>first, write your address</p>
+            <form class="form-inline">
+            <div class="form-group">
+                <label 
+                    class="sr-only" 
+                    for="inputEmail"
+                  >Email</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="inputEmail"
+                    placeholder="address@example.com">
+            </div>
+            <button type="submit" class="btn btn-default">Send</button>
+            </form>
+      </div>
  
 <!--links 2nd col-->
 
@@ -43,44 +35,41 @@
                         <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
                     </ul>       
         </div>
-    	
+      
 <!--links 3rd col-->
-    	<div class="col-sm-4 footer-section">
-    			<address>
-    			    <strong>Association des grimpeurs de bleau</strong><br>
-    			    222 rue de fontainebleau à Fontainebleau<br>
-       				 <a href="#">Contact Us</a>
-    			</address>
-			</div>
+      <div class="col-sm-4 footer-section">
+          <address>
+              <strong>Association des grimpeurs de bleau</strong><br>
+              222 rue de fontainebleau à Fontainebleau<br>
+               <a href="#">Contact Us</a>
+          </address>
+      </div>
 <!-- new row nav bar-->
-			<div class="row bottom-rule">
-    			<div class="col-sm-12">
-        			<nav class="navbar navbar-default navbar-footer">
-            			<ul class="nav navbar-nav">
-            			    <li><a href="#">Acceuil</a></li>
-            			    <li><a href="#">Site numéro 1</a></li>
-           				    <li><a href="#">site numéro 2</a></li>
-          				    <li><a href="#">SAE</a></li>
-            			    <li><a href="#">Evenements</a></li>
-                			<li><a href="#">Contact Us</a></li>
-            			</ul>
-        			</nav>
-    			</div>
+      <div class="row bottom-rule">
+          <div class="col-sm-12">
+              <nav class="navbar navbar-default navbar-footer">
+                  <ul class="nav navbar-nav">
+                      <li><a href="#">Acceuil</a></li>
+                      <li><a href="#">Site numéro 1</a></li>
+                      <li><a href="#">site numéro 2</a></li>
+                      <li><a href="#">SAE</a></li>
+                      <li><a href="#">Evenements</a></li>
+                      <li><a href="#">Contact Us</a></li>
+                  </ul>
+              </nav>
+          </div>
 <!-- Copyright -->
-   				<div class="row leg-room">
-    				<div class="col-md-12 text-center">
-    		    		<h1 class="text-uppercase">CLIMBING HEAVEN TERRITORY</h1>
-        				  <p class="monospaced">
-            			&copy;2017 WCS Dream Team Company Inc. 
-            			<span class="text-uppercase">All Rights Reserved</span></p>
-    				</div>
-				</div>
-			</div><!-- end row -->
+          <div class="row leg-room">
+            <div class="col-md-12 text-center">
+                <h1 class="text-uppercase">CLIMBING HEAVEN TERRITORY</h1>
+                  <p class="monospaced">
+                  &copy;2017 WCS Dream Team Company Inc. 
+                  <span class="text-uppercase">All Rights Reserved</span></p>
+            </div>
+        </div>
+      </div><!-- end row -->
    …
-  		</div>
- 	</div><!-- end container -->
+      </div>
+  </div><!-- end container -->
 </footer>
-	<!-- javascript bootstrap -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-</body>
-</html>
+  
