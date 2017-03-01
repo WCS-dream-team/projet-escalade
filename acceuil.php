cd5cddf095097aeb4b166a0006998990275e857f
Christophe_vinc <vincentchristophe177@gmail.com>
2017-03-01 15:39:40 +0100

    remise en place des div sur acceuil.csset PHp

--- acceuil.php
+++ acceuil.php
@@ -1,24 +1,57 @@
 
+
 	<!-- TRAM du Head-->
 <?php include("tram_head.php"); ?>
 
-
-	<body>
-
-		<!-- div class container pour appliquer bootstrap   --> 
-<!-- HEADER-->
+<body>
+  <!-- HEADER-->
 <?php include("header.php"); ?>
-<!-- SECTION ACTU-->
+  <!-- SECTION ACTU-->
 <?php include("section_actu.php"); ?>
-			
-		
-<!-- SECTION -->
-<?php include("section.php"); ?>
+ <!-- SECTION CARTE-->
+<div class="container-fluid">
 
-<!-- FOOTER -->
+	<div class="row">
+		<div class='col-md-12 sup_carte'>
+			<div class='carte'>		
+				<div class="row row_button"></div>
+				<div class="row row_button"></div>
+				<div class="row row_button"></div>
+				<div class="row row_button"></div>
+				<div class="row row_button">
+					<div class='col-md-offset-4 col-md-1'>
+					<a href="SNE.php"><button type="button" class="btn btn-primary" >Site d'apremont</button></a>
+				    </div>
+				</div>
+				<div class="row row_button"></div>
+				<div class="row row_button"></div>
+				<div class="row row_button"></div>
+				<div class="row row_button">
+					<div class='col-md-offset-6 col-md-1'>
+					<a href="SAE.php"><button type="button" class="btn btn-primary" >Salle d'escalade interieur</button></a>
+					</div>
+				</div>
+				<div class="row row_button"></div>
+				<div class="row row_button"></div>
+				<div class="row row_button">
+					<div class='col-md-offset-3 col-md-1'>
+    				<button type="button" class="btn btn-primary" >Les trois pignons</button>
+					</div>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+	
+
+   <!-- SECTION -->
+<?php include("section.php"); ?>
+   <!-- FOOTER -->
 <?php include("footer.php"); ?>
    <!-- javascript bootstrap -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-	</body>
+</body>
 </html>
