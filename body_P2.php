5583ce1057ec6f7c7f7a306fbdd64fdaf82bdbd5
GuimoO <guiguismoo@gmail.com>
2017-03-01 11:07:04 +0100

    git qui m'oblige à commit pour changer de branch...

---
+++ body_P2.php
@@ -0,0 +1,59 @@
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+  <!--lien bootstrap-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+  <!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+  <!--lien css-->
+  <LINK rel="stylesheet" type="text/css" href="header.css">
+  <LINK rel="stylesheet" type="text/css" href="acceuil_test.css">
+  <LINK rel="stylesheet" type="text/css" href="section_actu.css">
+  <LINK rel="stylesheet" type="text/css" href="section.css">
+  <LINK rel="stylesheet" type="text/css" href="footer.css">
+  <link rel="stylesheet" type="text/css" href="body_P2.css">
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+
+</head>
+  <body>
+      <div class="container-fluid">
+
+        <div class="row header_P2">
+
+            <div class="col-xs-12 col-sm-12  col-md-12 col-lg-12">
+
+              <h1 class="titre"><strong>3 PIGNONS</strong></h1>
+
+            </div>
+
+          <div/>
+
+
+
+
+
+          
+
+        <div class="row map">
+
+            <div class="col-xs-12 col-sm-12  col-md-12 col-lg-12">
+
+              <
+
+
+
+
+
+      </div>
+    
+      
+
+  
+<!-- FOOTER -->
+<?php include("footer.php"); ?>
+   <!-- javascript bootstrap -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+<html>
