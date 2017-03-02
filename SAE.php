64b5115a156751a34ab7359c314ff5ac2ed07bbc
Christophe_vinc <vincentchristophe177@gmail.com>
2017-03-02 09:23:10 +0100

    modif

---
+++ SAE.php
@@ -0,0 +1,46 @@
+
+
+	<!-- TRAM du Head-->
+<?php include("tram_head.php"); ?>
+
+<body>
+  <!-- HEADER-->
+<?php include("header.php"); ?>
+ 
+ <!-- SECTION CARTE-->
+<div class="container">
+	<div class="row">
+		<div class='col-md-12'>
+			<div class="row">
+				<div class='col-md-12'>
+				<a href="#" id="bloc">
+				<button type="button" class="btn btn-primary rangee">Site d'apremont</button>
+    			</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class='col-md-12'>
+				<a href="#" id="bloc">
+				<button type="button" class="btn btn-primary rangee">Les trois pignons</button>
+   				</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class='col-md-12'>
+				<a href="#" id="bloc">
+				<button type="button" class="btn btn-primary rangee">Salle d'escalade interieur</button>
+				</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>		
+
+   <!-- SECTION -->
+<?php include("section.php"); ?>
+   <!-- FOOTER -->
+<?php include("footer.php"); ?>
+   <!-- javascript bootstrap -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
