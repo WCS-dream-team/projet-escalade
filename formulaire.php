3cf3bce717a38e88f9694fd11f6e87985701a51d
Daf <daphne.bordalis@gmail.com>
2017-03-02 17:00:44 +0100

    vivement les vacs

---
+++ formulaire.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="formulaire.css">
+<LINK rel="stylesheet" type="text/css" href="footer.css">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+  <!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+</head>
+<body>
+
+<header>
+<div class="container-fluid">
+		<div class="row1">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 her">
+		
+					<h1 class="titre"><strong>ESCALADE<br>FORMULAIRE</strong></h1>
+				
+			</div>
+		</div>
+	</div>
+</header>
+
+<footer class="navbar-default">
+  <div class="container">
+  
+      <div class=”row bottom-rule”>
+      <!-- 1st col of the row -->
+        <div class="col-sm-4 footer-section">
+          <strong>Tell us more about you and ask a question</strong>
+          <p>first, write your address</p>
+            <form class="form-inline">
+            <div class="form-group">
+                <label 
+                    class="sr-only" 
+                    for="inputEmail"
+                  >Email</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="inputEmail"
+                    placeholder="address@example.com">
+            </div>
+            <button type="submit" class="btn btn-default">Send</button>
+            </form>
+      </div>
+ 
+<!--links 2nd col-->
+
+          <div class="col-sm-4 footer-section">
+                    <ul class="social-network social-circle">
+                        <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
+                        <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+                        <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>       
+        </div>
+      
+<!--links 3rd col-->
+      <div class="col-sm-4 footer-section">
+          <address>
+              <strong>Association des grimpeurs de bleau</strong><br>
+              222 rue de fontainebleau à Fontainebleau<br>
+               <a href="#">Contact Us</a>
+          </address>
+      </div>
+<!-- new row nav bar-->
+      <div class="row bottom-rule">
+          <div class="col-sm-12">
+              <nav class="navbar navbar-default navbar-footer">
+                  <ul class="nav navbar-nav">
+                      <li><a href="acceuil.php">Acceuil</a></li>
+                      <li><a href="SNE.php">Site numéro 1</a></li>
+                      <li><a href="#">site numéro 2</a></li>
+                      <li><a href="SAE.php">SAE</a></li>
+                      <li><a href="#">Evenements</a></li>
+                      <li><a href="formulaire.php">Contact Us</a></li>
+                  </ul>
+              </nav>
+          </div>
+<!-- Copyright -->
+          <div class="row leg-room">
+            <div class="col-md-12 text-center">
+                <h1 class="text-uppercase">CLIMBING HEAVEN TERRITORY</h1>
+                  <p class="monospaced">
+                  &copy;2017 WCS Dream Team Company Inc. 
+                  <span class="text-uppercase">All Rights Reserved</span></p>
+            </div>
+        </div>
+      </div><!-- end row -->
+   …
+      </div>
+  </div><!-- end container -->
+</footer>
+
+</body>
+
+	</html>
+
+
+  
