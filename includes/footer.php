4a7ebcb5de2aa89ae2d5b6b275c2a5c8cc739b27
GuimoO <guiguismoo@gmail.com>
2017-03-03 15:39:02 +0100

    copie colle footer body P2 sur l'include footer et suppression du footer sur bodyP2 av rajout include footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,80 +1,84 @@
-
-
 <footer class="navbar-default">
-  <div class="container">
-  
-      <div class=”row bottom-rule”>
-      <!-- 1st col of the row -->
-        <div class="col-sm-4 footer-section">
-          <strong>Tell us more about you and ask a question</strong>
-          <p>first, write your address</p>
-            <form class="form-inline">
-            <div class="form-group">
-                <label 
-                    class="sr-only" 
-                    for="inputEmail"
-                  >Email</label>
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="inputEmail"
-                    placeholder="address@example.com">
-            </div>
-            <button type="submit" class="btn btn-default">Send</button>
-            </form>
-      </div>
- 
-<!--links 2nd col-->
+        <div class="container">
+        
+            <div class="row bottom-rule">
+            <!-- 1st col of the row -->
+              <div class="col-sm-4 footer-section">
+                <strong>Tell us more about you and ask a question</strong>
+                <p>first, write your address</p>
+                <form class="form-inline">
+                  <div class="form-group">
+                      <label class="sr-only" for="inputEmail">Email</label>
+                      <input type="email" class="form-control" id="inputEmail" placeholder="address@example.com">
+                  </div>
+                  <button type="submit" class="btn btn-default">Send</button>
+                </form>
+              </div>
+       
+      <!--links 2nd col-->
+              <div class="col-sm-4 footer-section">
+                <ul class="social-network social-circle">
+                    <li>
+                      <a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a>
+                    </li>
+                    <li>
+                      <a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a>
+                    </li>
+                    <li>
+                      <a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a>
+                    </li>
+                    <li>
+                      <a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a>
+                    </li>
+                </ul>       
+              </div>
+            
+              <!--links 3rd col-->
+              <div class="col-sm-4 footer-section">
+                <address>
+                    <strong>Association des grimpeurs de bleau</strong><br>
+                    222 rue de fontainebleau à Fontainebleau<br>
+                     <a href="#">Contact Us</a>
+                </address>
+              </div>
+            </div> 
+      <!-- new row nav bar-->
+            <div class="row bottom-rule">
+                <div class="col-sm-12">
+                    <nav class="navbar navbar-default navbar-footer">
+                        <ul class="nav navbar-nav">
+                            <li><a href="acceuil.php">Acceuil</a></li>
+                            <li><a href="body_P2.php">Site numéro 1</a></li>
+                            <li><a href="#">site numéro 2</a></li>
+                            <li><a href="SAE.php">SAE</a></li>
+                            <li><a href="#">Evenements</a></li>
+                            <li><a href="formulaire.php">Contact Us</a></li>
+                        </ul>
+                    </nav>
+                </div>
+      <!-- Copyright -->
+                <div class="row leg-room">
+                  <div class="col-md-12 text-center">
+                      <h1 class="text-uppercase">CLIMBING HEAVEN TERRITORY</h1>
+                      <p class="monospaced">
+                        &copy;2017 WCS Dream Team Company Inc. 
+                        <span class="text-uppercase">All Rights Reserved</span>
+                      </p>
+                  </div>
+                </div>
+            </div><!-- end row -->
+        </div><!-- end container -->
+      </footer>
+    </div>
+  </div>
+</div>
 
-          <div class="col-sm-4 footer-section">
-                    <ul class="social-network social-circle">
-                        <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
-                        <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
-                        <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-                    </ul>       
-        </div>
-      
-<!--links 3rd col-->
-      <div class="col-sm-4 footer-section">
-          <address>
-              <strong>Association des grimpeurs de bleau</strong><br>
-              222 rue de fontainebleau à Fontainebleau<br>
-               <a href="#">Contact Us</a>
-          </address>
-      </div>
-<!-- new row nav bar-->
-      <div class="row bottom-rule">
-          <div class="col-sm-12">
-              <nav class="navbar navbar-default navbar-footer">
-                  <ul class="nav navbar-nav">
-                      <li><a href="index.php">Acceuil</a></li>
-                      <li><a href="SNE.php">Site numéro 1</a></li>
-                      <li><a href="#">site numéro 2</a></li>
-                      <li><a href="SAE.php">SAE</a></li>
-                      <li><a href="#">Evenements</a></li>
-                      <li><a href="formulaire.php">Contact Us</a></li>
-                  </ul>
-              </nav>
-          </div>
-<!-- Copyright -->
-          <div class="row leg-room">
-            <div class="col-md-12 text-center">
-                <h1 class="text-uppercase">CLIMBING HEAVEN TERRITORY</h1>
-                  <p class="monospaced">
-                  &copy;2017 WCS Dream Team Company Inc. 
-                  <span class="text-uppercase">All Rights Reserved</span></p>
-            </div>
-        </div>
-      </div><!-- end row -->
-   …
-      </div>
-  </div><!-- end container -->
-</footer>
-<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-   <!-- javascript bootstrap -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script type="text/javascript" src="assets/js/main.js"></script>
-</body>
-</html>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+<html>
